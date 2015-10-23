4389d8bc7886b679c8c2eabb6f7e790d953f748e
Harrison South <hsouth95@hotmail.co.uk>
2015-10-23 22:13:01 +0100

    Inital Commit

---
+++ Harrison.Database/DataAccess/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Harrison.Database.DataAccess
+{
+    using Model;
+    using System;
+    using System.Collections.Generic;
+
+    public interface IRepository<T> : IDisposable
+    {
+        IEnumerable<T> List { get; }
+        int Add(T entity);
+        void Delete(int id);
+        void Update(T entity);
+        T FindById(int id);
+    }
+}
